312f6a48af70b64d7a7c84a93f112ab08f74f641
ak21232 <ak21232@gmail.com>
2021-07-29 21:04:54 -0700

    Update

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -8,7 +8,27 @@ int main(){
   std::vector<std::string> words2 = readFileLine("test2.txt");
   bool assignmentTest = assignmentStatment(words);
   bool declaritiveTest = declarativeStatment(words2);
-  std::cout << "Assigned correctly?: " << assignmentTest <<'\n';
-  std::cout << "Declared correctly?: " << declaritiveTest << '\n';
+  if (assignmentTest == true){
+    for (int i = 1; i < words.size(); i++){
+      std::cout << words[i] << '\n';
+      std::cout << "<Statement> -> <Assign> <Assign> -> <ID> = <Expression>" << '\n';
+      std::cout << "Assigned correctly " <<'\n';
+    }
+  }
+  else {
+    std::cout << "Assigned incorrectly " <<'\n';
+  }
+
+  if (declaritiveTest == true){
+    for (int i = 1; i < words2.size(); i++){
+      std::cout << words2[i] << '\n';
+      std::cout << "<Statement> -> <Declarative> <Declarative> -> <Type> <ID> <Type> -> bool | float | int  " << '\n';
+      std::cout << "Declared correctly " <<'\n';
+    }
+  }
+  else {
+    std::cout << "Declaredincorrectly " <<'\n';
+  }
+
   return 0;
 }
